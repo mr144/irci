3cd0672e4a63749e43683068daa9a0b14e8708b2
epiii <gue.elfrianto@gmail.com>
2016-10-23 17:52:31 +0700

    home search reasul

--- irci_app/irci_app/App_Start/BundleConfig.cs
+++ irci_app/irci_app/App_Start/BundleConfig.cs
@@ -25,7 +25,8 @@ namespace irci_app
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/searchResult.css"));
         }
     }
 }
---
+++ irci_app/irci_app/Models/SearchResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace irci_app.Models
+{
+    public class SearchResult
+    {
+        public String Name
+        {
+            get
+            {
+
+            }
+        }
+    }
+}
